f127507b71167c53a9e57fc339e34953585eb9d1
mbarzegary <mbarzegary@msn.com>
2021-10-26 19:31:43 +0200

    add more comments to fenics code

--- fenics_tests/ns-diffusion.py
+++ fenics_tests/ns-diffusion.py
@@ -11,39 +11,39 @@ dt = T / num_steps # time step size
 mu = 0.001         # dynamic viscosity
 rho = 1            # density
 D = 0.01           # diffusion rate
-save_each = 10     # number of time steps to skip when saving
-plot_each = 20
+save_each = 10     # number of time steps to skip for saving vtk results
+plot_each = 20     # number of time steps to skip for saving plots
 
 # Create mesh
-channel = Rectangle(Point(0, 0), Point(2.0, 2.0))
-cylinder = Circle(Point(1.0, 1.0), 0.20)
-domain = channel - cylinder
-mesh = generate_mesh(domain, 64)
+channel = Rectangle(Point(0, 0), Point(2.0, 2.0)) # define the box
+cylinder = Circle(Point(1.0, 1.0), 0.20) # define the internal obstacle
+domain = channel - cylinder # create the domain by trimming the box
+mesh = generate_mesh(domain, 64) # generate the mesh
 
 # Define function spaces
-C = FunctionSpace(mesh, 'P', 1)
-V = VectorFunctionSpace(mesh, 'P', 2)
-Q = FunctionSpace(mesh, 'P', 1)
+C = FunctionSpace(mesh, 'P', 1) # for the temperature
+V = VectorFunctionSpace(mesh, 'P', 2) # for velocity (as a vector)
+Q = FunctionSpace(mesh, 'P', 1) # for pressure
 
 # Define boundaries
 inflow   = 'near(x[0], 0)'
 outflow  = 'near(x[0], 2.0)'
 walls    = 'near(x[1], 0) || near(x[1], 2.0)'
-cylinder = 'on_boundary && x[0]>0.6 && x[0]<1.4 && x[1]>0.6 && x[1]<1.4'
+cylinder = 'on_boundary && x[0]>0.6 && x[0]<1.4 && x[1]>0.6 && x[1]<1.4' # internal obstacle
 
 # Define inflow profile
-inflow_profile = ('4.0*1.5*x[1]*(2.0 - x[1]) / pow(2.0, 2)', '0')
+inflow_profile = ('4.0*1.5*x[1]*(2.0 - x[1]) / pow(2.0, 2)', '0') # u-shape profile of the inlet velocity
 
 # Define boundary conditions
-bcu_inflow = DirichletBC(V, Expression(inflow_profile, degree=2), inflow)
-bcu_walls = DirichletBC(V, Constant((0, 0)), walls)
-bcu_cylinder = DirichletBC(V, Constant((0, 0)), cylinder)
-bcp_outflow = DirichletBC(Q, Constant(0), outflow)
-bcu = [bcu_inflow, bcu_walls, bcu_cylinder]
-bcp = [bcp_outflow]
+bcu_inflow = DirichletBC(V, Expression(inflow_profile, degree=2), inflow) # fixed velocity BC on inlet
+bcu_walls = DirichletBC(V, Constant((0, 0)), walls) # no_slip BC on walls
+bcu_cylinder = DirichletBC(V, Constant((0, 0)), cylinder) # no_slip BC on obstacle
+bcp_outflow = DirichletBC(Q, Constant(0), outflow) # zero pressure BC on outlet
+bcu = [bcu_inflow, bcu_walls, bcu_cylinder] # NS velcoity BCs
+bcp = [bcp_outflow] # NS pressure BCs
 
-bc_c = DirichletBC(C, Constant(300.0), cylinder)
-bc_in = DirichletBC(C, Constant(450.0), inflow)
+bc_c = DirichletBC(C, Constant(300.0), cylinder) # constant temperate on obstacle
+bc_in = DirichletBC(C, Constant(450.0), inflow) # constant temperate on inlet
 bc = [bc_c, bc_in]
 
 # Define trial and test functions
@@ -55,11 +55,11 @@ c = TrialFunction(C)
 cv = TestFunction(C)
 
 # Define functions for solutions at previous and current time steps
-u_n = Function(V)
+u_n = Function(V) # implying zero initial conditions for velocities
 u_  = Function(V)
-p_n = Function(Q)
+p_n = Function(Q) # implying zero initial conditions for pressure
 p_  = Function(Q)
-c_n = interpolate(Expression("273.0", degree=2), C)
+c_n = interpolate(Expression("273.0", degree=2), C) # initial conditions for temperature
 
 # Define expressions used in variational forms
 U  = 0.5*(u_n + u)
@@ -107,14 +107,15 @@ A2 = assemble(a2)
 A3 = assemble(a3)
 
 # Apply boundary conditions to matrices
-[bc.apply(A1) for bc in bcu]
-[bc.apply(A2) for bc in bcp]
+[bc.apply(A1) for bc in bcu] # velocity BCs
+[bc.apply(A2) for bc in bcp] # pressure BCs
 
 # Create XDMF files for visualization output
-xdmffile_u = XDMFFile('output/velocity.xdmf')
-xdmffile_p = XDMFFile('output/pressure.xdmf')
-vtkfile_c = File('output/c.pvd')
+xdmffile_u = XDMFFile('output/velocity.xdmf') # velocity output file
+xdmffile_p = XDMFFile('output/pressure.xdmf') # pressure output file
+vtkfile_c = File('output/c.pvd') # temperature vtk output file
 
+# allow XDMF files be written after each step
 xdmffile_u.parameters["flush_output"] = True
 xdmffile_p.parameters["flush_output"] = True
 
@@ -122,6 +123,7 @@ xdmffile_p.parameters["flush_output"] = True
 progress = Progress('Looping', num_steps)
 # set_log_level(LogLevel.PROGRESS)
 
+# create figs directory for saving matplotlib plots
 if not os.path.isdir("figs"):
     os.makedirs("figs")
 
@@ -136,44 +138,47 @@ for n in range(num_steps):
 
     # Step 1: Tentative velocity step
     b1 = assemble(L1)
-    [bc.apply(b1) for bc in bcu]
-    solve(A1, u_.vector(), b1, 'bicgstab', 'hypre_amg')
+    [bc.apply(b1) for bc in bcu] # apply velocity BCs
+    solve(A1, u_.vector(), b1, 'bicgstab', 'hypre_amg') # solve velocity equation
 
     # Step 2: Pressure correction step
     b2 = assemble(L2)
-    [bc.apply(b2) for bc in bcp]
-    solve(A2, p_.vector(), b2, 'bicgstab', 'hypre_amg')
+    [bc.apply(b2) for bc in bcp] # apply pressure BCs
+    solve(A2, p_.vector(), b2, 'bicgstab', 'hypre_amg') # solve pressure equation
 
     # Step 3: Velocity correction step
     b3 = assemble(L3)
-    solve(A3, u_.vector(), b3, 'cg', 'sor')
+    solve(A3, u_.vector(), b3, 'cg', 'sor') # solve coupling equation
 
     # Solve the diffusion-convection equation
     solve(a4 == L4, c, bc)
 
     # Save solution to file
-    if (n % save_each == 0):
+    if (n % save_each == 0): # if it's time to write output:
         xdmffile_u.write(u_, t)
         xdmffile_p.write(p_, t)
         vtkfile_c << (c, t)
 
-    if (n % plot_each == 0):
+    if (n % plot_each == 0): # if it's time to plot pressure and temperature profiles
+        # plot pressure profile on inlet
         tol = 0.001  # avoid hitting points outside the domain
-        y = np.linspace(0 + tol, 2.0 - tol, 101)
-        points = [(0, y_) for y_ in y]
-        p_line = np.array([p_(point) for point in points])
-        plt.plot(y, p_line, 'b', linewidth=2)
+        y = np.linspace(0 + tol, 2.0 - tol, 101) # generate y value of points at which evaluation occurs
+        points = [(0, y_) for y_ in y] # generate the set of points in the inlet
+        p_line = np.array([p_(point) for point in points]) # evaluate the value of pressure on generated points
+        plt.plot(y, p_line, 'b', linewidth=2) # plot pressure profile in blue
         plt.xlabel('$y$')
-        plt.savefig(f'figs/p_curve-{n}.png')
+        plt.savefig(f'figs/p_curve-{n}.png') # save the plot
         plt.clf()
 
-        points = [(2.0, y_) for y_ in y]
-        t_line = np.array([c(point) for point in points])
-        plt.plot(y, t_line, 'r', linewidth=2)
+        # plot temperate profile on outlet
+        points = [(2.0, y_) for y_ in y] # generate the set of points in the outlet
+        t_line = np.array([c(point) for point in points]) # evaluate the value of temperature on generated points
+        plt.plot(y, t_line, 'r', linewidth=2) # plot temperature profile in red
         plt.xlabel('$y$')
-        plt.savefig(f'figs/t_curve-{n}.png')
+        plt.savefig(f'figs/t_curve-{n}.png') # save the plot
         plt.clf()
 
+        # compute and print the weighted average value of pressure (inlet) and temperature (outlet)
         p_average = np.average(p_line)
         c_average = np.average(t_line)
         print(f"=====> Averaged pressure at inlet: {p_average} <=====")
